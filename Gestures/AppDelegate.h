8e61033b2f38808a191ceae28cc093da8f0631a8
Shreve-La <shrevela@gmail.com>
2017-06-13 11:19:27 -0400

    Initial Commit

---
+++ Gestures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gestures
+//
+//  Created by swcl on 2017-06-13.
+//  Copyright © 2017 Shreve.ca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
