07c62fa7ace35feb8769fc245dabc26a021bf1ae
Shreve-La <shrevela@gmail.com>
2017-06-13 14:41:33 -0400

    Added a tap gesture tab and tap gesture recognizer

--- Gestures/TapViewController.m
+++ Gestures/TapViewController.m
@@ -14,5 +14,31 @@
 
 @implementation TapViewController
 
+-(void)viewDidLoad{
+
+[super viewDidLoad];
+CGFloat width = 100;
+CGFloat height = 100;
+CGRect frame = CGRectMake(CGRectGetMidX(self.view.bounds)-width/2, CGRectGetMidY(self.view.bounds)-height/2, width, height);
+
+UIView *view = [[UIView alloc] initWithFrame:frame];
+
+view.backgroundColor = [UIColor grayColor];
+[self.view addSubview:view];
+
+UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(viewTapped:)];
+    
+[view addGestureRecognizer:tapGesture];
+
+}
+
+-(void)viewTapped:(UITapGestureRecognizer *)sender{
+    
+    UIColor *color = [sender.view.backgroundColor isEqual:[UIColor grayColor]] ? [UIColor magentaColor] : [UIColor grayColor];
+    
+    sender.view.backgroundColor = color;
+
+}
+
 
 @end
