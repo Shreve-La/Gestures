a1f57ba5be9039061abfd1c784dff0c7c4f3831c
Shreve-La <shrevela@gmail.com>
2017-06-13 13:34:21 -0400

    added third scene to interface builder

--- Gestures/PanViewController.m
+++ Gestures/PanViewController.m
@@ -15,10 +15,16 @@
 @implementation PanViewController
 
 - (IBAction)redViewPanned:(UIPanGestureRecognizer *)sender {
-    CGPoint locationInView =[sender locationInView:self.view];
-    sender.view.center = locationInView;
-    
-    
+// One way to add a pan gesture:
+//    CGPoint locationInView =[sender locationInView:self.view];
+//    sender.view.center = locationInView;
+
+// Second way to add a pan gesture:
+    CGPoint translationInView = [sender translationInView:self.view];
+    CGPoint oldCenter = sender.view.center;
+    CGPoint newCenter = CGPointMake(oldCenter.x + translationInView.x,  oldCenter.y + translationInView.y);
+    sender.view.center = newCenter;
+    [sender setTranslation:CGPointZero inView:self.view];
     
 }
 
---
+++ Gestures/PinchViewController.m
@@ -0,0 +1,17 @@
+//
+//  PinchViewController.m
+//  Gestures
+//
+//  Created by swcl on 2017-06-13.
+//  Copyright © 2017 Shreve.ca. All rights reserved.
+//
+
+#import "PinchViewController.h"
+
+@interface PinchViewController ()
+
+@end
+
+@implementation PinchViewController
+
+@end
